62ecf9854d56cf816e21e6254002c83fd0f896a0
Toastt39 <tonkao84444@gmail.com>
2024-10-01 16:21:35 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+prefix = "!"
+limit = 120 
+channelid = 1290510947953283103
+channelLog = 1290512018662756455
+
+server = [1277823510831431694]
+ownerIds = [1084379989354479646]
+LogTopup = 1290511007713595392
+phone = "0821923995"
+roleSettings = [
+    {
+        "roleId": 1290531846970408992,
+        "price": 30
+    }
+]
+
+ha = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
+}
